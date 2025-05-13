7b1f8514e241c803242fad7fda3a5219d7b46cb0
ShukurullaHackaton1 <sayidabdullatursinbayev205@gmail.com>
2025-05-13 17:59:20 +0500

    remove donwload btn from piechart

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -73,12 +73,6 @@ export default function PieActiveArc({ height, data, title = "Pie Chart" }) {
           height={height}
         />
       </div>
-      <button
-        onClick={exportToExcel}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
-      >
-        Export to Excel
-      </button>
     </div>
   );
 }
--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://teacher-portfolio-server.vercel.app";
+axios.defaults.baseURL = "https://tutor-app-server-gules.vercel.app/";
 axios.interceptors.request.use((option) => {
   const token = localStorage.getItem("admin-jwt")
     ? localStorage.getItem("admin-jwt")
